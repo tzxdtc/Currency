a31ef026beeb8c18d3057c30552adf495efc25df
tzxdtc <tzxdtc10@gmail.com>
2020-08-29 13:55:11 +0900

    Initial Commit

---
+++ currency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  currency
+//
+//  Created by 小牛 on 2020/08/29.
+//  Copyright © 2020 小牛. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ currencyTests/currencyTests.swift
@@ -0,0 +1,34 @@
+//
+//  currencyTests.swift
+//  currencyTests
+//
+//  Created by 小牛 on 2020/08/29.
+//  Copyright © 2020 小牛. All rights reserved.
+//
+
+import XCTest
+@testable import currency
+
+class currencyTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
